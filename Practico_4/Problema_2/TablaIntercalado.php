c41603c1fda7efa409aa40b98cff873e6a78903c
Tumiri Huanca Alex <zelechos@hotmail.com>
2020-03-23 14:59:27 -0400

    Practico SIS 256 Version 4.3.0

--- Practico_4/Problema_2/TablaIntercalado.php
+++ Practico_4/Problema_2/TablaIntercalado.php
@@ -1,63 +1,57 @@
-
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Ejercicio 2</title>
-    <link rel="stylesheet" type="text/css" href="estilo.css">
-  </head>
-  <body>
-    <form action="TablaIntercalado.php" method="post" autocomplete="off">
-      <h1>Digite una Cantidad de Columnas y Filas para mostrar la Bandera Boliviana Y Chuquisaqueña : </h1>
-      <p>Digite el numero de Columnas</p>
-      <input type="text" name="Columnas"><br>
-      <p>Digite el numero de Filas</p>
-      <input type="text" name="Filas"><br><br><br>
-    <div >
-      <input type="submit" value="Mostrar">
-    </div>
-    </form>
-    <table align ="left">
-    <?php
+<html>
+<head>
+	<title>Ejercicio</title>
+  <link rel="stylesheet" type="text/css" href="estilo.css">
 
-    $Columnas =$_POST['Columnas'];
-    $Filas =$_POST['Filas'];
-    $s=0;
+</head>
+<body>
+  <form action="TablaIntercalado.php" method="POST" autocomplete="off">
+    <p>Digite el Numero de Filas : </p>
+    <input type="number" name="Filas"><br>
+    <p>Digite el Numero de Columnas : </p>
+    <input type="number" name="Columnas"><br><br><br>
+    <input type="submit" value="Enviar" >
 
-for ($i=0; $i < $Columnas/2; $i++) {
+  </form>
 
-  switch ($s) {
-    case 0:
-      for ($c=0; $c < $Filas; $c++) {
-        if($c %3 == 0){
-          echo "<tr bgcolor = #F90000 >";
-        }else if($c%2==0 && $c != 4 || $c %5 ==0){
-          echo "<tr bgcolor = #35A920>";
-        }else{
-          echo "<tr bgcolor = #F6FA0E>";
-        }
-          echo "<td><br></td>";
-        echo "</tr>";
-      }
-    echo "</table>";
-      $s++;
+	<?php
+     function Tablas($Filas,$Columnas){
+     	echo "<table align = 'center'>" ;
+     	for($k=1;$k<=$Filas;$k++){
+           echo "<tr height='27'>";
+     		for($j=1;$j<=$Columnas;$j++){
 
-      case 1:
-      echo "<table align ='right'>";
-      for ($c=0; $c < $Filas; $c++) {
-        if($c %2 == 0){
-          echo "<tr bgcolor = #FFFFFF >";
-        }else{
-          echo "<tr bgcolor = #F90000>";
-        }
-          echo "<td><br></td>";
-        echo "</tr>";
-      }
-    }
-    $s--;
-  }
-     ?>
-   </table>
+     			if($j%2==0){
+     			switch($k%2)
+			{
+			case 0:echo "<td width='270' bgcolor='red'></td>";
+			  break;
+			case 1:echo "<td width='270' bgcolor='white'></td>";
+			  break;
+			  }
+     			}
+     			else{
+     			switch($k%3)
+			{
+			case 0:echo "<td width='270' bgcolor='green'></td>";
+			  break;
+			case 1:echo "<td width='270' bgcolor='red'></td>";
+			  break;
+			case 2:echo "<td width='270' bgcolor='yellow'></td>";
+			  break;
+			  }
+     			}
+     		}
+     	   echo"</tr>";
+     	}
+     }
+     $Filas=$_POST['Filas'];
+     $Columnas=$_POST['Columnas'];
 
-  </body>
+     Tablas($Filas,$Columnas);
+
+    ?>
+
+</body>
 </html>
